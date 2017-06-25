a33ae887074a602e9f0e96c41dbbbb4cf12be83d
James Le <le_j6@denison.edu>
2017-06-24 17:21:12 -0700

    Destructuring Imports

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -2,14 +2,12 @@
 
 // Import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 // Create a component
-const App = () => {
-  return (
+const App = () => (
     <Text>Some Text</Text>
-  );
-};
+);
 
 // Render it to the device
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('albums', () => App);
