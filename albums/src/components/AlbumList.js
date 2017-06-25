8709577e0e5652624a35e5f1f6f4a66237f34d82
James Le <le_j6@denison.edu>
2017-06-25 01:30:20 -0700

    Displaying Individual Albums

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -10,11 +11,16 @@ class AlbumList extends Component {
       .then(response => this.setState({ albums: response.data }));
   }
 
+  renderAlbums() {
+    this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />);
+  }
+
   render() {
     console.log(this.state);
     return (
       <View>
-        <Text>Album List!!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
