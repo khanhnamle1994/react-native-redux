12a4e5d589ceaffe4aaa909301a5565c0c525cb6
James Le <le_j6@denison.edu>
2017-06-24 18:11:38 -0700

    Component Level State

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,11 +3,15 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>Album List!!!!</Text>
