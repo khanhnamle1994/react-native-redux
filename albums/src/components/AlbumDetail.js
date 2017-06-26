ddf6204177380ae1e9a0ae951d7d1242ac3240ce
James Le <le_j6@denison.edu>
2017-06-26 15:24:11 -0700

    Handling User Input with Buttons

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -26,13 +27,18 @@ const AlbumDetail = ({ album }) => {
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
-        </CardSection>
-        <CardSection>
-          <Image
-            style={imageStyle}
-            source={{ uri: image }}
-          />
-        </CardSection>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button />
+      </CardSection>
     </Card>
   );
 };
