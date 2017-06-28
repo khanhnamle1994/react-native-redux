e045b779b37983096027d234e4aede57754ff1af
James Le <le_j6@denison.edu>
2017-06-28 00:39:21 -0700

    Handling Authentication Events

--- auth/src/app.js
+++ auth/src/app.js
@@ -5,6 +5,8 @@ import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAHCQpdUWRYyF_nQMFo8D3BTxndZUYS6nA',
@@ -13,6 +15,14 @@ class App extends Component {
       projectId: 'authentication-b18bf',
       storageBucket: 'authentication-b18bf.appspot.com',
       messagingSenderId: '453776208495'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
 
