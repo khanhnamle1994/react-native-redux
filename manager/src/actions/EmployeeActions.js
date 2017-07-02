0819b4254d5fa842d2d2bd437672903b8cee3190
James Le <le_j6@denison.edu>
2017-07-02 16:46:41 -0700

    Employee Delete Action Creator

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -50,3 +50,16 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
       });
   };
 };
+
+
+export const employeeDelete = ({ uid }) => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .remove()
+      .then(() => {
+        Actions.employeeList({ type: 'reset' });
+      });
+  };
+};
--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -25,6 +25,14 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
   }
 
+  onAccept() {
+    
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+
   render() {
     return (
       <Card>
@@ -47,7 +55,11 @@ class EmployeeEdit extends Component {
           </Button>
         </CardSection>
 
-        <Confirm visible={this.state.showModal}>
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
           Are you sure you want to delete this?
         </Confirm>
       </Card>
