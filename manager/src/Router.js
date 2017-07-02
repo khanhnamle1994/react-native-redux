95baba0d359afc1714d147a6f642e86dd40f1014
James Le <le_j6@denison.edu>
2017-07-01 19:50:14 -0700

    Navigation Bar Buttons

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -11,7 +11,13 @@ const RouterComponent = () => {
       </Scene>
 
       <Scene key="main">
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+        <Scene
+          onRight={() => console.log('right!!!')}
+          rightTitle="Add"
+          key="employeeList"
+          component={EmployeeList}
+          title="Employees"
+        />
       </Scene>
     </Router>
   );
